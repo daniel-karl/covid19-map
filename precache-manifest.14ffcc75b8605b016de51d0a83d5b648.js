ce6c060c67542275c178879354e163ed2bfa82b7
daniel-karl <daniel.karl@ibm.com>
2020-04-01 08:53:18 -0400

    Updates

--- precache-manifest.14ffcc75b8605b016de51d0a83d5b648.js
+++ precache-manifest.14ffcc75b8605b016de51d0a83d5b648.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "593b8914aa04548dbc98985b77904d74",
+    "revision": "c401dae5e5b465b84d351d4a3c7c836f",
     "url": "/index.html"
   },
   {
-    "revision": "cc38f91d6c0582f99151",
+    "revision": "7dc75d3b29e4533c68ce",
     "url": "/static/css/2.6d072544.chunk.css"
   },
   {
-    "revision": "3c8cb13fd724b88bfd02",
+    "revision": "9d85587c2a76e0d25104",
     "url": "/static/css/main.41cb3584.chunk.css"
   },
   {
-    "revision": "cc38f91d6c0582f99151",
-    "url": "/static/js/2.beae1014.chunk.js"
+    "revision": "7dc75d3b29e4533c68ce",
+    "url": "/static/js/2.95c3fa6c.chunk.js"
   },
   {
     "revision": "46277a134c0da755c65dea6590ca0296",
-    "url": "/static/js/2.beae1014.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.95c3fa6c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3c8cb13fd724b88bfd02",
-    "url": "/static/js/main.6697feb6.chunk.js"
+    "revision": "9d85587c2a76e0d25104",
+    "url": "/static/js/main.3f92bda4.chunk.js"
   },
   {
     "revision": "ce1077e492625482b243",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56bfb8cb3ebd75a17f34d16ae5aee4f0.js"
+  "/precache-manifest.14ffcc75b8605b016de51d0a83d5b648.js"
 );
 
 self.addEventListener('message', (event) => {
